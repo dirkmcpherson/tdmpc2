3deed9277183cac2b4bb45b8a79a4e1d39b7043e
James Staley <jstale02@p1cmp016.pax.tufts.edu>
2024-09-15 12:31:29 -0400

    hpc training scripts

---
+++ myjob.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -J tdmpc2pusht_noHD #job name
+#SBATCH --time=01-00:00:00 #requested time (DD-HH:MM:SS)
+#SBATCH -p gpu #running on "mpi" partition/queue
+#SBATCH --gres=gpu:a100:1 #requesting 1 GPU
+#SBATCH --constraint="a100-80G"
+#SBATCH -N 1 #1 nodes
+#SBATCH -n 8 #2 tasks total (default 1 CPU core per task) = # of cores
+#SBATCH --mem=64g #RAM total
+#SBATCH --output=MyJob.%j.%N.out #saving standard output to file, %j=JOBID, %N=NodeName
+#SBATCH --error=MyJob.%j.%N.err #saving standard error to file, %j=JOBID, %N=NodeName
+#SBATCH --mail-type=ALL #email optitions
+#SBATCH --mail-user=james.staley625703@tufts.edu 
+
+
+#[commands_you_would_like_to_exe_on_the_compute_nodes] 
+# for example, running a python script
+# 1st, load the modulemodule load anaconda/2021.05
+# run pythonpython myscript.py #make sure myscript.py exists in the current directory or provide thefull path to script
+
+module load anaconda/2021.11
+module load cuda/12.2
+export WANDB_DATA_DIR=/cluster/tufts/shortlab/jstale02
+export WANDB_CACHE_DIR=/cluster/tufts/shortlab/jstale02
+sleep 30
+source activate three_ten
+python tdmpc2/train.py task=pusht obs=rgb demo_path=''
