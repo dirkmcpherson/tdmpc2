afdac63d125f92d41876f41fb2d86f79291ce490
j <dirkmcpherson@gmail.com>
2026-02-24 11:43:38 -0500

    better train script

--- tdmpc2/ondeck_ms.sh
+++ tdmpc2/ondeck_ms.sh
@@ -1,6 +1,6 @@
-for i in {3..8}; do
+for i in {1..5}; do
     echo "**************Run $i***************"
-    python train.py task=pick-cube model_size=5 wandb_project=tdmpc2 wandb_entity=jambotime demo_dir=demonstrations/motionplanning/ seed=$i &
+    python train.py task=pick-cube model_size=5 wandb_project=tdmpc2 wandb_entity=jambotime obs=rgb second_cam=rgb seed=$(shuf -i 0-1073741824 -n 1) demo_dir=demonstrations/teleop/rgb_rgb uncertainty_weight=true &
     sleep 1
-    python train.py task=pick-cube model_size=5 wandb_project=tdmpc2 wandb_entity=jambotime demo_dir=demonstrations/motionplanning/ seed=$((i*10)) obs=rgb
+    python train.py task=pick-cube model_size=5 wandb_project=tdmpc2 wandb_entity=jambotime obs=rgb second_cam=rgb seed=$(shuf -i 0-1073741824 -n 1) demo_dir=demonstrations/teleop/rgb_rgb
 done
